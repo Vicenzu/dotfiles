3594ad1bfd1f3b78151a7bd6c67b7d3a50067ce8
Vicenzu <vincicalogero2004@gmail.com>
2026-03-02 14:50:43 +0100

    update dotfiles

--- scripts/chargeLimit.sh
+++ scripts/chargeLimit.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 if [ "$1" -ne "$(cat /sys/class/power_supply/BAT1/charge_control_end_threshold)" ]; then
-	sudo asusctl -c $1;
+	sudo asusctl battery limit $1;
 	carica=$(cat /sys/class/power_supply/BAT1/charge_control_end_threshold);
 	echo "La carica è impostata al $carica%";
 else
