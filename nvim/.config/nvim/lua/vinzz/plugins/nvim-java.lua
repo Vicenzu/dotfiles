ce17fdec35a77d48afee682fb63775e3b091a6a2
Vicenzu <vincicalogero2004@gmail.com>
2026-04-25 22:41:15 +0200

    update dotfiles

--- nvim/.config/nvim/lua/vinzz/plugins/nvim-java.lua
+++ nvim/.config/nvim/lua/vinzz/plugins/nvim-java.lua
@@ -1,6 +1,15 @@
-return { 'nvim-java/nvim-java',
-  config = function()
-    require('java').setup()
-    vim.lsp.enable('jdtls')
-  end,
+return {
+	"nvim-java/nvim-java",
+	config = function()
+		require("java").setup({
+			jdtls = {
+				settings = {
+					jvm_args = {
+						"-javaagent:/usr/share/java/lombok.jar",
+					},
+				},
+			},
+		})
+		vim.lsp.enable("jdtls")
+	end,
 }
